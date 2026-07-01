bc1689ecafc4697b97d8f6470d6db30b212a60da
EmilyMyaskovski <memilysda10@gmail.com>
2026-07-01 22:00:38 +0300

    Remove unused code

--- main.py
+++ main.py
@@ -71,15 +71,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Print Guard API", lifespan=lifespan)
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.include_router(auth_router)
 
 # --- Schemas ---
